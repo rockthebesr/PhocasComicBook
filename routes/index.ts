a9ba0d5bd66953fb0a5524a4f86b2f7cb2d43ba1
RockLira <shiqi.rock.luo@gmail.com>
2016-01-27 01:34:32 -0800

    First commit

---
+++ routes/index.ts
@@ -0,0 +1,95 @@
+///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
+
+///<reference path='../types/DefinitelyTyped/express/express.d.ts'/>
+
+interface UserInterface {
+  getName(): string;
+  getEmail(): string;
+
+}
+class User implements UserInterface {
+  private name;
+  private email;
+
+  constructor(name: string, email:string) {
+    this.name = name;
+    this.email = email;
+  }
+  getName(): string {
+    return this.name;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+}
+class Router {
+  router;
+
+  constructor() {
+
+    var express = require('express');
+    var router = express.Router();
+
+    /* GET home page. */
+    router.get('/', function(req, res, next) {
+      res.render('index', { title: 'Express' });
+    });
+
+    /* GET Hello World page. */
+    router.get('/helloworld', function(req, res) {
+      res.render('helloworld', { title: 'Hello, World!' });
+    });
+
+    /* GET Userlist page. */
+    router.get('/userlist', function(req, res) {
+      var db = req.db;
+      var collection = db.get('usercollection');
+      collection.find({},{},function(e,docs){
+        res.render('userlist', {
+          "userlist" : docs
+        });
+      });
+    });
+
+    /* GET New User page. */
+    router.get('/newuser', function(req, res) {
+      res.render('newuser', { title: 'Add New User' });
+    });
+
+
+    /* POST to Add User Service */
+    router.post('/adduser', function(req, res) {
+
+      // Set our internal DB variable
+      var db = req.db;
+
+      // Get our form values. These rely on the "name" attributes
+      var newUser = new User(req.body.username, req.body.useremail);
+
+      // Set our collection
+      var collection = db.get('usercollection');
+
+      // Submit to the DB
+      collection.insert({
+        "username" : newUser.getName(),
+        "email" : newUser.getEmail()
+      }, function (err, doc) {
+        if (err) {
+          // If it failed, return error
+          res.send("There was a problem adding the information to the database.");
+        }
+        else {
+          // And forward to success page
+          res.redirect("userlist");
+        }
+      });
+    });
+
+    this.router = router;
+  }
+
+}
+var router = new Router();
+module.exports = router.router;
