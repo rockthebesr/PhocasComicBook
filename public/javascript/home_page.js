196c662da277743d2ee6f463e381aa9ecd3c108a
RockLira <shiqi.rock.luo@gmail.com>
2016-02-11 02:48:03 -0800

    adding buttons for manage page

--- public/javascript/home_page.js
+++ public/javascript/home_page.js
@@ -1,13 +1,13 @@
 ///<reference path='jquery.d.ts'/>
-    jQuery(document).on('click', '#comicSetImage', function(){
-        imgRedirect();
-    });
-    
-    var imgRedirect = function(){
-        // if (setTitle.length > 0) {
-        //     window.loctaion = '/comic_page/' + setTitle;
-        // } else {
-        //     alert("Cannot retrieve this comic set");
-        // }   
-        alert(setTitle);
-    }
+jQuery(document).on('click', '#comicSetImage', function () {
+    imgRedirect();
+});
+var imgRedirect = function () {
+    // if (setTitle.length > 0) {
+    //     window.loctaion = '/comic_page/' + setTitle;
+    // } else {
+    //     alert("Cannot retrieve this comic set");
+    // }   
+    alert("test");
+};
+//# sourceMappingURL=home_page.js.map
